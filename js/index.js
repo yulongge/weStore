5c43a58a316b0fe433671ac41329626ac0a81165
yulongge <geyulong@163.com>
2015-04-13 18:31:01 +0800

    事件
    菜单点击事件，个人

--- js/index.js
+++ js/index.js
@@ -4,5 +4,18 @@ weStore.init = function(argument) {
                 e.preventDefault();
                 e.stopPropagation();
      }, false);
-}
+};
+weStore.loadPage=function($container,url){
+	var urlStr = url+".html";
+	$container.load(urlStr,function(){
+		
+	});
+};
+
 weStore.init();
+
+$(function(){
+	$(".myfooter .menu li a").tap(function(){
+		weStore.loadPage($(".mymain"),$(this).attr("value"));
+	});
+});
