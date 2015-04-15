6fe4f91fadeea85dc2d5bfa7e63eef8bb9efa146
yulongge <geyulong@163.com>
2015-04-15 14:30:07 +0800

    first.html
    摇一摇惊喜

--- js/index.js
+++ js/index.js
@@ -11,6 +11,10 @@ weStore.loadPage=function($container,url){
 		
 	});
 };
+weStore.sharkEvent=function(){
+	alert(3);
+};
+
 
 weStore.init();
 
@@ -18,4 +22,38 @@ $(function(){
 	$(".myfooter .menu li a").tap(function(){
 		weStore.loadPage($(".mymain"),$(this).attr("value"));
 	});
+
+	weStore.loadPage($(".mymain"),$(".myfooter .menu li a").eq(0).attr("value"));
 });
+
+//摇一摇
+var SHAKE_THRESHOLD = 3000;
+var last_update = 0;
+var x = y = z = last_x = last_y = last_z = 0;
+function sharkInit() {
+    if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion', deviceMotionHandler, false);
+    } else {
+        weStore.sharkEvent();
+    }
+}
+function deviceMotionHandler(eventData) {
+    var acceleration = eventData.accelerationIncludingGravity;
+    var curTime = new Date().getTime();
+    if ((curTime - last_update) > 100) {
+        var diffTime = curTime - last_update;
+        last_update = curTime;
+        x = acceleration.x;
+        y = acceleration.y;
+        z = acceleration.z;
+        var speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+        if (speed > SHAKE_THRESHOLD) {
+            $(".msgCon").text("Welcome")
+            
+        }
+        last_x = x;
+        last_y = y;
+        last_z = z;
+    }
+}
